cc20ce45964cab7f26d5a7497822c91f3aa8d21f
James Waring <jwaring@M1-3646.local>
2025-08-22 15:21:46 -0400

    Changed to an iterator

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,13 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
-        System.out.println("What is your name? ");
         Scanner scanner = new Scanner(System.in);
-        String name = scanner.nextLine();
-        System.out.println("Hello "+name);
+        System.out.println("Choose a number ");
+        int number = scanner.nextInt();
+
+        for(int i = 0; i < number; i++){
+            System.out.println("I can count to "+ (i+1));
+        }
+
     }
 }
